72956b61652e1ec7c073e9abab0ae318257881ec
Brandon Lara <190776@utags.edu.mx>
2020-11-26 13:41:26 -0600

    Creacion de una carpeta de rutas

--- server/server.js
+++ server/server.js
@@ -9,41 +9,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
  
 // parse application/json
 app.use(bodyParser.json());
- 
-app.get('/', function (req, res) {
+
+app.get('/', function(req, res) {
   res.send('<h1>Bienvenido a mi servidor REST</h1>');
 });
- 
-app.get('/usuario', function (req, res) {
-    res.json({
-      ok: 200,
-      mensaje: 'Usuarios consultados con exito'
-    })
-});
 
-app.post('/usuario', function (req, res) {
-  let nombre = req.body.nombre;
-  let body = req.body;
-
-  res.json({
-    ok: 200,
-    mensaje: 'Usuario insertado con exito',
-    body: body
-
-  });
-});
-  
-app.put('/usuario/:id/:nombre', function(req,res){
-  let id = req.params.id;
-  let nombre = req.params.nombre;
-
-  res.json({
-    ok: 200,
-    mensaje: 'Usuario  actualizado con exito',
-    id: id,
-    nombre: nombre
-  });
-});
+app.use(require('./routes/usuario'));
 
 mongoose.connect('mongodb:0//localhost:27017/cafeteria', {
   useNewUrlParser: true,
